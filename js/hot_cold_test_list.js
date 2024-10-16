51feec76b992e47dc297a46a6984184975ba23a2
ifs21014@students.del.ac.id <ifs21014@students.del.ac.id>
2024-10-16 10:01:26 +0700

    feat:hot cold test list

--- js/hot_cold_test_list.js
+++ js/hot_cold_test_list.js
@@ -475,7 +475,7 @@ function hot_cold_test_list_submit(result_status) {
     var row_hot_cold_test_list_detail = $('#hot_cold_test_list_hot_cold_test_list_detail').datagrid('getRows');
     //alert(arr.length); 
     if ((row !== null) && (row_hot_cold_test_list_detail.length>0)){
-        $.messager.confirm('Submit Submited', 'After submited you can not change the hardnesstest item anymore<br/><br/><center>Are you sure?</center>', function (r) {
+        $.messager.confirm('Submit Submited', 'After submited you can not change the droptest item anymore<br/><br/><center>Are you sure?</center>', function (r) {
             if (r) {
                 $.post(base_url + 'hot_cold_test_list/submit', {id: row.id,result_s:result_status}, function (result) {
                     if (result.success) {
@@ -491,7 +491,7 @@ function hot_cold_test_list_submit(result_status) {
             }
         });
     } else {
-        $.messager.alert('Submitted hardness Test  Warning', 'No hardness Test or no item to be submitted', 'warning');
+        $.messager.alert('Submitted Drop Test  Warning', 'No Drop Test or no item to be submitted', 'warning');
     }
 }
 function print_hot_cold_test_list(type,view_type) {
